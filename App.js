d995d1a3cd8b4690c9b40bfc91eb7b0581af82d0
Vinh Huynh <vinh-huynh@outlook.com>
2024-02-15 11:39:48 -0800

    Added test welcome screen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './src/screens/WelcomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <WelcomeScreen />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,10 @@
+import { Button, View, Text } from "react-native";
+
+export default function WelcomeScreen() {
+    return (
+        <View>
+            <Text>LearnFitness</Text>
+            <Button title="Login"></Button>
+        </View>
+    ) 
+}
