6572b87d760ba7bc8fba2a59e9611f04fd453a3d
Vinh Huynh <vinh-huynh@outlook.com>
2024-04-13 21:11:30 -0700

    IN WorkoutsScreen, merge 2 separate useEffects (which do the same thing, but called twice). Removed WorkoutModal (now in a separate component). Improved logic of workout press handler.

--- screens/home/WorkoutsScreen.jsx
+++ screens/home/WorkoutsScreen.jsx
@@ -4,42 +4,16 @@ import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, ScrollView, Image, Pressable, Alert, ActivityIndicator, Modal, TouchableOpacity } from "react-native";
 import LinearBackground from "../../components/LinearBackground";
 import { getBackendData } from "./../../utils/backendAPI";
+import { WorkoutModal } from "../../components/WorkoutModal";
 
 export default function WorkoutsScreen({ route, navigation }) {
   const [loading, setLoading] = useState(false);
   const [workouts, setWorkouts] = useState([]);
   const [recommendedWorkouts, setRecommendedWorkouts] = useState([]);
   const [selectedWorkout, setSelectedWorkout] = useState(null);
+  const [isWorkoutModalVisible, setWorkoutModalVisible] = useState(false);
 
-  async function getWorkoutsAsync() {
-    setLoading(true);
-    try {
-      const userDocRef = await firestore().collection("users").doc(auth().currentUser.uid).get();
-      const userExp = userDocRef.data().expLevel;
-      const workouts = await getBackendData("user/workouts");
-      const premadeWorkoutsSnapshot = await firestore().collection("premade_workouts").where("expLevel", "==", userExp.toLowerCase()).get();
-
-      let recommendedWorkoutsArray = [];
-      premadeWorkoutsSnapshot.forEach((workout) => {
-        recommendedWorkoutsArray.push(workout.data());
-      })
-
-      setWorkouts(workouts);
-      setRecommendedWorkouts(recommendedWorkoutsArray);
-    } catch (error) {
-      Alert.alert(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    async function getWorkouts() {
-      await getWorkoutsAsync();
-    }
-    getWorkouts();
-  }, []);
-
+  // Get workouts from Firestore
   useEffect(() => {
     const unsubscribe = firestore()
       .collection('users')
@@ -47,7 +21,25 @@ export default function WorkoutsScreen({ route, navigation }) {
       .collection("workouts")
       .onSnapshot(() => {
         async function getWorkouts() {
-          await getWorkoutsAsync();
+          setLoading(true);
+          try {
+            const userDocRef = await firestore().collection("users").doc(auth().currentUser.uid).get();
+            const userExp = userDocRef.data().expLevel;
+            const workouts = await getBackendData("user/workouts");
+            const premadeWorkoutsSnapshot = await firestore().collection("premade_workouts").where("expLevel", "==", userExp.toLowerCase()).get();
+
+            let recommendedWorkoutsArray = [];
+            premadeWorkoutsSnapshot.forEach((workout) => {
+              recommendedWorkoutsArray.push(workout.data());
+            })
+
+            setWorkouts(workouts);
+            setRecommendedWorkouts(recommendedWorkoutsArray);
+          } catch (error) {
+            Alert.alert(error.message);
+          } finally {
+            setLoading(false);
+          }
         }
         getWorkouts();
       });
@@ -56,16 +48,39 @@ export default function WorkoutsScreen({ route, navigation }) {
     return () => unsubscribe();
   }, []);
 
-  const handleWorkoutPress = (workout, isModify = false) => {
-    if (isModify) {
-      navigation.navigate("AddWorkoutScreen", { workout }); // Navigate to AddWorkoutScreen with workout data
-    } else {
-      setSelectedWorkout(workout);
-    }
+  function handleCloseModal() {
+    setWorkoutModalVisible(false);
+  }
+
+  const handleAddWorkout = () => {
+    navigation.navigate("AddWorkoutScreen", { headerTitle: "Create workout", action: "create" });
   };
 
-  const handleAddWorkoutPlan = () => {
-    navigation.navigate("AddWorkoutScreen");
+
+  const WorkoutItem = ({ workout, isYourWorkout }) => {
+    const handleQuickView = () => {
+      setSelectedWorkout(workout);
+      setWorkoutModalVisible(true);
+    };
+
+    const generateBriefDescription = () => {
+      if (workout.exercises.length === 1) {
+        return `• ${workout.exercises[0].sets} x ${workout.exercises[0].name}`;
+      } else {
+        const firstTwoExercises = workout.exercises.slice(0, 2).map(exercise => `• ${exercise.sets} x ${exercise.name}`);
+        return `${firstTwoExercises.join("\n")} \n...`;
+      }
+    };
+
+    return (
+      <TouchableOpacity activeOpacity={0.7} style={styles.workoutItemContainer} onPress={handleQuickView}>
+        <Image source={require("./../../assets/workout_plans_images/leg1.jpg")} style={styles.workoutImage} />
+        <View style={styles.workoutDetailsContainer}>
+          <Text style={styles.workoutName}>{workout.name}</Text>
+          <Text style={styles.workoutDescription}>{generateBriefDescription()}</Text>
+        </View>
+      </TouchableOpacity>
+    );
   };
 
   return (
@@ -76,12 +91,13 @@ export default function WorkoutsScreen({ route, navigation }) {
             <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
               <Text style={styles.title}>Your Workouts</Text>
               <View style={styles.workoutsContainer}>
-                {workouts.length === 0 ? <Text style={{ color: "darkgrey", fontSize: 17 }}>You have no workouts yet</Text> :
+                {workouts.length === 0 ?
+                  <Text style={{ color: "darkgrey", fontSize: 17 }}>You have no workouts yet</Text>
+                  :
                   workouts.map((workout, index) => (
                     <WorkoutItem
                       key={index}
                       workout={workout}
-                      onPress={handleWorkoutPress}
                       isYourWorkout={true}
                     />
                   )
@@ -95,111 +111,27 @@ export default function WorkoutsScreen({ route, navigation }) {
                     <WorkoutItem
                       key={index}
                       workout={workout}
-                      onPress={handleWorkoutPress}
                     />
                   )
                   )}
               </View>
 
             </ScrollView>
-            <TouchableOpacity style={styles.addButton} onPress={handleAddWorkoutPlan}>
+            <TouchableOpacity activeOpacity={0.4} style={styles.addButton} onPress={handleAddWorkout}>
               <Text style={styles.addButtonText}>+</Text>
             </TouchableOpacity>
 
-            {selectedWorkout && (
-              <WorkoutDetailsModal
-                workout={selectedWorkout}
-                onClose={() => setSelectedWorkout(null)}
-                navigation={navigation}
-              />
-            )}
+            <WorkoutModal
+              workout={selectedWorkout}
+              navigation={navigation}
+              isWorkoutModalVisible={isWorkoutModalVisible}
+              handleCloseModal={handleCloseModal}
+            />
           </>
         )}
-
     </LinearBackground>
   );
 }
-
-const WorkoutDetailsModal = ({ workout, onClose, navigation }) => {
-  const handleModifyWorkout = () => {
-    onClose(); 
-    navigation.navigate("AddWorkoutScreen", { workout });
-  };
-
-  const handleDeleteWorkout = async () => {
-    try {
-      await firestore()
-        .collection('users')
-        .doc(auth().currentUser.uid)
-        .collection('workouts')
-        .doc(workout.id) 
-        .delete();
-      onClose(); 
-    } catch (error) {
-      console.error("Error deleting workout:", error);
-    }
-  };
-
-  return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={true}
-      onRequestClose={onClose}
-    >
-      <Pressable style={styles.modalContainer} onPress={onClose}>
-        <View style={styles.modalContent}>
-          <Pressable onPress={onClose} style={styles.closeButton}>
-            <Text style={{ color: "blue" }}>Close</Text>
-          </Pressable>
-          <Text style={styles.modalHeader}>Workout Summary</Text>
-          <Text style={styles.workoutName}>{workout.name}</Text>
-          {workout.exercises.map((exercise, index) => (
-            <Text key={index}>• {exercise.sets} x {exercise.name}</Text>
-          ))}
-          <View style={styles.buttonsContainer}>
-            <Pressable onPress={handleModifyWorkout} style={[styles.button, styles.modifyButton]}>
-              <Text style={{ color: "blue" }}>Modify</Text>
-            </Pressable>
-            <Pressable onPress={handleDeleteWorkout} style={[styles.button, styles.deleteButton]}>
-              <Text style={{ color: "red" }}>Delete</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Pressable>
-    </Modal>
-  );
-};
-
-
-const WorkoutItem = ({ workout, onPress, isYourWorkout }) => {
-  const handleQuickView = () => {
-    onPress(workout); 
-  };
-
-  const generateBriefDescription = () => {
-    if (workout.exercises.length === 0) {
-      return "No exercises available";
-    } else if (workout.exercises.length === 1) {
-      return `• ${workout.exercises[0].sets} x ${workout.exercises[0].name}`;
-    } else {
-      const firstTwoExercises = workout.exercises.slice(0, 2).map(exercise => `• ${exercise.sets} x ${exercise.name}`);
-      return `${firstTwoExercises.join("\n")} \n...`;
-    }
-  };
-
-  return (
-    <View style={styles.workoutItemContainer}>
-      <Pressable onPress={handleQuickView}>
-        <Image source={require("./../../assets/workout_plans_images/leg1.jpg")} style={styles.workoutImage} />
-        <View style={styles.workoutDetailsContainer}>
-          <Text style={styles.workoutName}>{workout.name}</Text>
-          <Text style={styles.workoutDescription}>{generateBriefDescription()}</Text>
-        </View>
-      </Pressable>
-    </View>
-  );
-};
 
 const styles = StyleSheet.create({
   container: {
@@ -218,29 +150,24 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   workoutItemContainer: {
-    overflow: "hidden",
-    width: "47%",
+    width: "48%",
     borderRadius: 10,
-    backgroundColor: "rgba(255, 255, 255, 1)",
-    marginBottom: 10
+    backgroundColor: "white",
+    marginBottom: 14
   },
   workoutImage: {
     width: '100%',
-    height: 170,
+    height: 150,
     alignSelf: "center",
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10
   },
   workoutDetailsContainer: {
-    paddingHorizontal: 10,
-    paddingBottom: 10
+    padding: 10
   },
   workoutName: {
     fontSize: 18,
     fontWeight: "700"
-  },
-  showMoreButton: {
-    paddingVertical: 5,
   },
   addButton: {
     position: "absolute",
@@ -251,56 +178,21 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "teal"
+    backgroundColor: "teal",
+
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 2.22,
+
+    elevation: 3,
   },
   addButtonText: {
-    color: "rgba(0, 0, 0, 0.3)",
+    color: "rgba(0, 0, 0, 0.6)",
     fontSize: 30,
-    fontWeight: "bold",
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-  modalContent: {
-    backgroundColor: "#fff",
-    padding: 20,
-    borderRadius: 10,
-    width: "80%",
-  },
-  modalHeader: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  closeButton: {
-    position: "absolute",
-    top: 10,
-    right: 10,
-  },
-  workoutName: {
-    fontSize: 18,
-    fontWeight: "700"
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 10,
-  },
-  button: {
-    flex: 1,
-    alignItems: 'center',
-    paddingVertical: 10,
-    borderRadius: 5,
-  },
-  modifyButton: {
-    backgroundColor: '#e0e0e0', 
-    marginRight: 5,
-  },
-  deleteButton: {
-    backgroundColor: '#ffcdd2', 
-    marginLeft: 5,
   },
 });
