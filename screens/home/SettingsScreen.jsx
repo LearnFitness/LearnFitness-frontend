70edd12bb63ae56333341e324d1453fe0b31ba36
Ayush Patel <AyushHarishkumar.Patel@student.csulb.edu>
2024-04-18 16:52:20 -0700

    edit profile page
    The backend is not ready yet. frontend complete

--- screens/home/SettingsScreen.jsx
+++ screens/home/SettingsScreen.jsx
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { View, SafeAreaView, Text, ActivityIndicator, StyleSheet, Alert, StatusBar, Pressable, Modal, Switch } from "react-native";
+import { View, SafeAreaView, Text, ActivityIndicator, StyleSheet, Alert, StatusBar, Pressable, Modal, Switch, TextInput, Button } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import auth from "@react-native-firebase/auth";
 import { getBackendDataWithRetry, postBackendDataWithPhoto } from "../../utils/backendAPI";
 import { useIsFocused } from '@react-navigation/native';
 import AvatarPicker from "../../components/AvatarPicker";
+import { ButtonGroup, Divider } from "@rneui/themed";
 
 export default function SettingsScreen() {
   const [userData, setUserData] = useState(null);
   const [photoObject, setPhotoObject] = useState(null);
   const [loading, setLoading] = useState(false);
   const [showNotificationSettings, setShowNotificationSettings] = useState(false);
+  const [showEditProfileettings, setShowEditProfileSettings] = useState(false);
   const [notificationsEnabled, setNotificationsEnabled] = useState(true);
   const [soundEnabled, setSoundEnabled] = useState(true);
   const [vibrationEnabled, setVibrationEnabled] = useState(true);
+
+  const [editprofileData, setEditProfileData] = useState({
+    height: "",
+    weight: "",
+    gender: "",
+    expLevel: "",
+  });
+  const [selectedGender,  setSelectedGender] = useState(0);
+  const genders = ["Male", "Female", "Other"]
+  const [selectedLevel, setSelectedLevel] = useState(0);
+  const levels = ["Beginner", "Intermediate", "Expert"];
 
   // Modifies status bar color ONLY on Settings screen
   function FocusAwareStatusBar(props) {
@@ -28,6 +41,14 @@ export default function SettingsScreen() {
         const userData = await getBackendDataWithRetry("/user");
         setUserData(userData);
         setPhotoObject({ uri: userData.photoURL })
+        setEditProfileData({
+          height: userData.height,
+          weight: userData.weight,
+          gender: userData.gender,
+          expLevel: userData.expLevel,
+        })
+        setSelectedGender(genders.indexOf(userData.gender))
+        setSelectedLevel(levels.indexOf(userData.expLevel));
       } catch (error) {
         Alert.alert("An error occurred", error);
       } finally {
@@ -71,9 +92,18 @@ export default function SettingsScreen() {
     setShowNotificationSettings(!showNotificationSettings);
   };
 
+  const toggleEditProfileSettings = () => {
+    console.log("Edit Profile")
+    setShowEditProfileSettings(!showEditProfileettings);
+  };
+
   return (
     <View style={styles.container}>
-      <FocusAwareStatusBar translucent backgroundColor="transparent" barStyle={"dark-content"} />
+      <FocusAwareStatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle={"dark-content"}
+      />
       <View style={styles.header}>
         <Text style={styles.headerText}>Settings</Text>
       </View>
@@ -83,31 +113,51 @@ export default function SettingsScreen() {
         <View style={styles.contentContainer}>
           {/* Profile picture, name, and email */}
           <View style={styles.profileContainer}>
-            <AvatarPicker photoObject={photoObject.uri ? { uri: photoObject.uri } : null} setPhotoObject={handleEditPhoto} />
-            <Text style={styles.profileName} >{userData.name.split(" ")[0]}</Text>
+            <AvatarPicker
+              photoObject={photoObject.uri ? { uri: photoObject.uri } : null}
+              setPhotoObject={handleEditPhoto}
+            />
+            <Text style={styles.profileName}>
+              {userData.name.split(" ")[0]}
+            </Text>
             <Text style={styles.profileEmail}>{userData.email}</Text>
           </View>
           <View style={styles.horizontalLine}></View>
           {/* Buttons */}
           <View style={styles.buttonsContainer}>
             {/* To be implemented (User information like weight, height, etc.) */}
-            <Pressable onPress={() => console.log("Edit profile")}>
+            <Pressable onPress={toggleEditProfileSettings}>
               <View style={styles.buttonRow}>
-                <Feather name="user" size={24} color="black" style={styles.icon} />
+                <Feather
+                  name="user"
+                  size={24}
+                  color="black"
+                  style={styles.icon}
+                />
                 <Text style={styles.button}>Edit Profile</Text>
               </View>
             </Pressable>
             {/* Notifications settings */}
             <Pressable onPress={toggleNotificationSettings}>
               <View style={styles.buttonRow}>
-                <Feather name="bell" size={24} color="black" style={styles.icon} />
+                <Feather
+                  name="bell"
+                  size={24}
+                  color="black"
+                  style={styles.icon}
+                />
                 <Text style={styles.button}>Notifications</Text>
               </View>
             </Pressable>
             {/* Logout button */}
             <Pressable onPress={handleLogout}>
               <View style={styles.buttonRow}>
-                <Feather name="log-out" size={24} color="black" style={styles.icon} />
+                <Feather
+                  name="log-out"
+                  size={24}
+                  color="black"
+                  style={styles.icon}
+                />
                 <Text style={styles.button}>Logout</Text>
               </View>
             </Pressable>
@@ -116,35 +166,154 @@ export default function SettingsScreen() {
       )}
       {/* Notifications settings tab */}
       <Modal
-      animationType="slide"
-      // transparent={true}
-      visible={showNotificationSettings}
-      onRequestClose={toggleNotificationSettings}>
-      <SafeAreaView style={styles.modalContainer}>
-        <View style={styles.notificationSettingsContainer}>
-          <View style={styles.notificationsHeader}>
-            <Pressable onPress={toggleNotificationSettings}>
-              <Feather name="arrow-left" size={26} color="black" />
-            </Pressable>
-            <Text style={styles.notificationsHeader}>   Notifications</Text>
+        animationType="slide"
+        // transparent={true}
+        visible={showNotificationSettings}
+        onRequestClose={toggleNotificationSettings}
+      >
+        <SafeAreaView style={styles.modalContainer}>
+          <View style={styles.notificationSettingsContainer}>
+            <View style={styles.notificationsHeader}>
+              <Pressable onPress={toggleNotificationSettings}>
+                <Feather name="arrow-left" size={26} color="black" />
+              </Pressable>
+              <Text style={styles.notificationsHeader}> Notifications</Text>
+            </View>
+            {/* Buttons */}
+            <View style={styles.notificationSettings}>
+              <Text style={styles.notificationText}>General notifications</Text>
+              <Switch
+                value={notificationsEnabled}
+                onValueChange={setNotificationsEnabled}
+              />
+            </View>
+            <View style={styles.notificationSettings}>
+              <Text style={styles.notificationText}>Sound</Text>
+              <Switch value={soundEnabled} onValueChange={setSoundEnabled} />
+            </View>
+            <View style={styles.notificationSettings}>
+              <Text style={styles.notificationText}>Vibration</Text>
+              <Switch
+                value={vibrationEnabled}
+                onValueChange={setVibrationEnabled}
+              />
+            </View>
           </View>
-          {/* Buttons */}
-          <View style={styles.notificationSettings}>
-            <Text style={styles.notificationText}>General notifications</Text>
-            <Switch value={notificationsEnabled} onValueChange={setNotificationsEnabled} />
-          </View>
-          <View style={styles.notificationSettings}>
-            <Text style={styles.notificationText}>Sound</Text>
-            <Switch value={soundEnabled} onValueChange={setSoundEnabled} />
-          </View>
-          <View style={styles.notificationSettings}>
-            <Text style={styles.notificationText}>Vibration</Text>
-            <Switch value={vibrationEnabled} onValueChange={setVibrationEnabled} />
-          </View>
-        </View>
-      </SafeAreaView>
-    </Modal>
+        </SafeAreaView>
+      </Modal>
 
+      <Modal
+        animationType="slide"
+        // transparent={true}
+        visible={showEditProfileettings}
+        onRequestClose={toggleEditProfileSettings}
+      >
+        <SafeAreaView style={styles.modalContainer}>
+          <View style={styles.notificationSettingsContainer}>
+            <View style={styles.notificationsHeader}>
+              <Pressable onPress={toggleEditProfileSettings}>
+                <Feather name="arrow-left" size={26} color="black" />
+              </Pressable>
+              <Text style={styles.notificationsHeader}> Edit Profile</Text>
+            </View>
+            <View>
+              <View style={[styles.editcontainer, { flexDirection: "row" }]}>
+                <Text style={styles.edittitle}>Name</Text>
+                <TextInput
+                  editable={false}
+                  style={styles.inputComponent}
+                  value={userData && userData.name ? userData.name : ""}
+                />
+              </View>
+              <Divider />
+              <View style={[styles.editcontainer, { flexDirection: "row" }]}>
+                <Text style={styles.edittitle}>Age</Text>
+                <TextInput
+                  editable={false}
+                  style={styles.inputComponent}
+                  value={userData && userData.age ? userData.age : ""}
+                />
+              </View>
+              <Divider />
+              <View style={[styles.editcontainer, { flexDirection: "row" }]}>
+                <Text style={styles.edittitle}>Email</Text>
+                <TextInput
+                  editable={false}
+                  style={styles.inputComponent}
+                  value={userData && userData.email ? userData.email : ""}
+                />
+              </View>
+              <Divider />
+              <View style={[styles.editcontainer, { flexDirection: "row" }]}>
+                <Text style={styles.edittitle}>Height (ft.in)</Text>
+                <TextInput
+                  onChangeText={(e) => {
+                    setEditProfileData({ ...editprofileData, height: e });
+                  }}
+                  style={styles.inputComponent}
+                  value={editprofileData.height || ""}
+                />
+              </View>
+              <Divider />
+              <View style={[styles.editcontainer, { flexDirection: "row" }]}>
+                <Text style={styles.edittitle}>Weight (lbs)</Text>
+                <TextInput
+                  onChangeText={(e) => {
+                    setEditProfileData({ ...editprofileData, weight: e });
+                  }}
+                  style={styles.inputComponent}
+                  value={editprofileData.weight || ""}
+                />
+              </View>
+              <Divider />
+              <View style={{ flexDirection: "col" }}>
+                <Text style={styles.edittitle}> Gender</Text>
+                <ButtonGroup
+                  buttons={genders}
+                  selectedIndex={selectedGender}
+                  onPress={(value) => {
+                    setSelectedGender(value);
+                    setEditProfileData({
+                      ...editprofileData,
+                      gender: genders[value],
+                    });
+                  }}
+                  containerStyle={{ marginBottom: 20 }}
+                />
+              </View>
+              <Divider />
+              <View style={{ flexDirection: "col" }}>
+                <Text style={styles.edittitle}> Experience Level</Text>
+                <ButtonGroup
+                  buttons={levels}
+                  selectedIndex={selectedLevel}
+                  onPress={(value) => {
+                    setSelectedLevel(value);
+                    setEditProfileData({
+                      ...editprofileData,
+                      expLevel: levels[value],
+                    });
+                  }}
+                  containerStyle={{ marginBottom: 20 }}
+                />
+              </View>
+            </View>
+            <View style={{ marginTop: 90, backgroundColor: "#007AFF" }}>
+              <Button
+                title="Save Changes"
+                color="#fff"
+                onPress={() => Alert.alert("Simple Button pressed")}
+                disabled={
+                  editprofileData.expLevel === "" ||
+                  editprofileData.weight === "" ||
+                  editprofileData.height === "" ||
+                  editprofileData.gender === ""
+                }
+              />
+            </View>
+          </View>
+        </SafeAreaView>
+      </Modal>
     </View>
   );
 }
@@ -219,6 +388,17 @@ const styles = StyleSheet.create({
     alignItems: "baseline",
     marginTop: 10,
     marginBottom: 20,
+  },
+  editcontainer: {
+    height: 50,
+    alignItems: "center",
+    justifyContent: "space-between",
+    margin: 5,
+  },
+  edittitle: {
+    fontWeight: "600",
+    fontSize: 20,
+    margin: 5,
   },
   notificationSettingsContainer: {
     backgroundColor: "white",
