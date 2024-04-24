3a6f5a6dbf55981b5527535a3925673a10c0aed7
Vinh Huynh <vinh-huynh@outlook.com>
2024-04-23 22:15:57 -0700

    AddWorkout now adhere better to React guidelines with immutable state updates

--- screens/AddWorkoutScreen.jsx
+++ screens/AddWorkoutScreen.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { View, ScrollView, Text, StyleSheet, Alert, TextInput, TouchableOpacity } from "react-native";
 import LinearBackground from "../components/LinearBackground";
-import ExerciseSets from "../components/ExerciseSets";
 import FontAwesome from "react-native-vector-icons/FontAwesome6";
 import firestore from "@react-native-firebase/firestore";
 import auth from "@react-native-firebase/auth";
@@ -13,22 +12,22 @@ export default function AddWorkoutScreen({ route, navigation }) {
   const [workoutId, setWorkoutId] = useState("");
   const [workoutName, setWorkoutName] = useState("");
   const [workoutDescription, setWorkoutDescription] = useState("");
-  const [selectedExercises, setSelectedExercises] = useState([]);
+  const [exercises, setExercises] = useState([]);
 
   useEffect(() => {
     if (exercise) {
-      addExercise(exercise);
+      handleAddExercise(exercise);
     }
     if (workout) {
       setWorkoutId(workout.id);
       setWorkoutName(workout.name);
       setWorkoutDescription(workout.description);
-      setSelectedExercises(workout.exercises);
+      setExercises(workout.exercises);
     }
   }, [exercise, workout]);
 
   function handleGoBack() {
-    if (!workoutName && selectedExercises.length === 0) {
+    if (!workoutName && exercises.length === 0) {
       navigation.goBack();
     } else {
       Alert.alert(
@@ -51,26 +50,46 @@ export default function AddWorkoutScreen({ route, navigation }) {
     }
   }
 
-  function addExercise(exercise) {
-    const exerciseExists = selectedExercises.some((ex) => ex.id === exercise.id);
+  function handleAddExercise(exercise) {
+    const exerciseExists = exercises.some((ex) => ex.id === exercise.id);
     if (!exerciseExists) {
-      const initialSets = workout ? exercise.sets : 1; // If modifying, use the existing sets value
-      setSelectedExercises((prevExercises) => [...prevExercises, { ...exercise, sets: initialSets }]);
+      const sets = workout ? exercise.sets : 1; // If modifying, use the existing sets value
+      setExercises(prevExercises => [...prevExercises, { ...exercise, sets }]);
     } else {
       Alert.alert("Exercise already added to the workout.");
     }
   }
 
-  function removeExercise(exerciseId) {
-    setSelectedExercises((prevExercises) => prevExercises.filter((ex) => ex.id !== exerciseId));
+  function handleRemoveExercise(exerciseId) {
+    setExercises(prevExercises => prevExercises.filter((ex) => ex.id !== exerciseId));
   }
+
+  function handleAddSet(exerciseId) {
+    const updatedExercises = exercises.map(ex => {
+      if (ex.id === exerciseId) {
+        return { ...ex, sets: ex.sets + 1 };
+      }
+      return ex;
+    })
+    setExercises(updatedExercises);
+  };
+
+  function handleRemoveSet(exerciseId) {
+    const updatedExercises = exercises.map(ex => {
+      if (ex.id === exerciseId && ex.sets > 1) {
+        return { ...ex, sets: ex.sets - 1 };
+      }
+      return ex;
+    })
+    setExercises(updatedExercises);
+  };
 
   async function handleSaveWorkout() {
     if (!workoutName.trim()) {
       Alert.alert("Workout name is required.");
       return;
     }
-    if (selectedExercises.length === 0) {
+    if (exercises.length === 0) {
       Alert.alert("Please add exercises to your workout.");
       return;
     }
@@ -79,7 +98,7 @@ export default function AddWorkoutScreen({ route, navigation }) {
       const newWorkout = {
         name: workoutName,
         description: workoutDescription,
-        exercises: selectedExercises
+        exercises: exercises
       };
 
       if (action === "edit") {
@@ -124,18 +143,20 @@ export default function AddWorkoutScreen({ route, navigation }) {
           </TouchableOpacity>
         </View>
 
-        {selectedExercises.map((exercise) => (
-          <View key={exercise.id}>
-            <Ionicon
-              name="close-circle-outline"
-              color="#aa3155"
-              size={23}
-              style={{ position: "absolute", zIndex: 2, right: 15, top: 28 }}
-              onPress={() => removeExercise(exercise.id)}
-            />
-            <ExerciseSets exercise={exercise} />
-          </View>
-        ))}
+        {exercises ?
+          exercises.map((exercise) => (
+            <View key={exercise.id}>
+              <Ionicon
+                name="close-circle-outline"
+                color="#aa3155"
+                size={23}
+                style={{ position: "absolute", zIndex: 2, right: 15, top: 28 }}
+                onPress={() => handleRemoveExercise(exercise.id)}
+              />
+              <ExerciseSets exercise={exercise} />
+            </View>
+          ))
+          : null}
 
         <TouchableOpacity style={styles.addExerciseButton} onPress={() => navigation.navigate("ExercisesSearchModal")}>
           <FontAwesome name="plus" color="white" size={23} />
@@ -144,9 +165,50 @@ export default function AddWorkoutScreen({ route, navigation }) {
       </ScrollView>
     </LinearBackground>
   );
+
+  function ExerciseSets({ exercise }) {
+    return (
+      <View style={styles.setsContainer}>
+        <TouchableOpacity onPress={() => navigation.navigate("ExerciseModal", { exerciseId: exercise.id })}>
+          <Text style={styles.exerciseName}>{exercise.name}</Text>
+        </TouchableOpacity>
+        <Text style={styles.exerciseTarget}>{exercise.target}</Text>
+        <View style={styles.exerciseHeader}>
+          <Text style={[{ flex: 1, marginLeft: 15 }, styles.exerciseHeaderText]}>Set</Text>
+          <Text style={[{ flex: 2 }, styles.exerciseHeaderText]}>lbs</Text>
+          <Text style={[{ flex: 2 }, styles.exerciseHeaderText]}>Reps</Text>
+        </View>
+        <View>
+          {[...Array(exercise.sets)].map((value, index) => {
+            return <Set key={index} setIndex={index} exercise={exercise} />
+          })}
+        </View>
+        <TouchableOpacity style={styles.addSetButton} onPress={() => handleAddSet(exercise.id)}>
+          <Text style={styles.addSetButtonText}>+ Add Set</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function Set({ exercise, setIndex }) {
+    return (
+      <View style={styles.exerciseSet}>
+        <Ionicon
+          name="remove-circle-outline"
+          color="#aa3155"
+          size={17}
+          onPress={() => handleRemoveSet(exercise.id)}
+        />
+        <Text style={{ flex: 1, textAlign: "center", fontSize: 15 }}>{setIndex + 1}</Text>
+        <TextInput editable={false} style={[{ flex: 2 }, styles.exerciseInput]} />
+        <TextInput editable={false} style={[{ flex: 2 }, styles.exerciseInput]} />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
+  // AddWorkoutScreen
   container: {
     marginHorizontal: "5%"
   },
@@ -193,5 +255,60 @@ const styles = StyleSheet.create({
     color: "darkgrey",
     fontSize: 17,
     margin: 5
+  },
+
+  // Sets
+  setsContainer: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 15,
+    marginTop: 15
+  },
+  exerciseName: {
+    color: "blue",
+    fontSize: 17,
+    fontWeight: "600",
+    textAlign: "center",
+    textTransform: "capitalize",
+    marginBottom: 5,
+    marginHorizontal: "5%"
+  },
+  exerciseTarget: {
+    color: "grey",
+    textAlign: "center",
+    textTransform: "capitalize",
+    marginBottom: 5
+  },
+  exerciseHeader: {
+    flexDirection: "row",
+  },
+  exerciseHeaderText: {
+    textAlign: "center",
+    fontSize: 15,
+    fontWeight: "600",
+    alignItems: "center"
+  },
+  exerciseSet: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 5
+  },
+  exerciseInput: {
+    textAlign: "center",
+    backgroundColor: "rgb(245, 245, 245)",
+    padding: 3,
+    marginHorizontal: 3,
+    borderRadius: 5,
+    fontSize: 15
+  },
+  addSetButton: {
+    alignSelf: "center",
+    backgroundColor: "lightgrey",
+    width: "40%",
+    padding: 3,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 5
   }
 });
