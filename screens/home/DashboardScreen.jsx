36df8ec0d51b14b30a931373e1cb358517814a7e
Thai Tran <Thai.Tran01@student.csulb.edu>
2024-04-23 23:17:30 -0700

    the most important ui update ever

--- screens/home/DashboardScreen.jsx
+++ screens/home/DashboardScreen.jsx
@@ -24,9 +24,9 @@ function getRandomSentence() {
 
 function getGreeting() {
   const hour = new Date().getHours();
-  if (hour < 12) return "Good morning";
-  if (hour < 18) return "Good afternoon";
-  return "Good evening";
+  if (hour < 12) return "Good morning,";
+  if (hour < 18) return "Good afternoon,";
+  return "Good evening,";
 }
 
 export default function DashboardScreen({ navigation }) {
