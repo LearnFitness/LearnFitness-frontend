b331e90c37473baaacf02d5ecd33ebff27f15c70
Thai Tran <Thai.Tran01@student.csulb.edu>
2024-05-06 19:22:52 -0700

    created total workouts per week chart, fetched data to display for total minutes spent per day working out, ui changes to progress page and dashboard greeting

--- screens/home/ProgressScreen.jsx
+++ screens/home/ProgressScreen.jsx
@@ -9,6 +9,7 @@ import {
   StyleSheet,
   ActivityIndicator,
   Dimensions,
+  ScrollView
 } from "react-native";
 import LinearBackground from "../../components/LinearBackground";
 import auth from "@react-native-firebase/auth";
@@ -18,13 +19,16 @@ import firestore from "@react-native-firebase/firestore";
 import { Calendar, CalendarList } from "react-native-calendars";
 import { getBackendDataWithRetry } from "../../utils/backendAPI"
 import { useEffect, useState } from "react";
+import FontAwesome from "react-native-vector-icons/FontAwesome6"
+import moment from 'moment';
 import {
   LineChart,
   BarChart,
   PieChart,
   ProgressChart,
   ContributionGraph,
-  StackedBarChart
+  StackedBarChart,
+  ChartTitle
 } from "react-native-chart-kit";
 
 export default function ProgressScreen({ navigation }) {
@@ -33,9 +37,11 @@ export default function ProgressScreen({ navigation }) {
   const [showHistory, setHistory] = useState(false);
   const [completedWorkoutDates, setCompletedWorkoutDates] = useState({});
   const [totalWorkouts, setTotalWorkouts] = useState(0);
-  const [avgWorkoutsPerWeek, setAvgWorkoutsPerWeek] = useState(0);
   const [avgWorkoutDuration, setAvgWorkoutDuration] = useState(0);
   const [caloriesBurnedPerWeek, setCaloriesBurnedPerWeek] = useState(0);
+  const [workoutTimePerDay, setWorkoutTimePerDay] = useState([0, 0, 0, 0, 0, 0, 0]); 
+  const [maxWorkoutTime, setMaxWorkoutTime] = useState(0);
+  const [weeklyWorkouts, setWeeklyWorkouts] = useState([]);
 
   useEffect(() => {
     const unsubscribe = firestore()
@@ -67,16 +73,21 @@ export default function ProgressScreen({ navigation }) {
         const dates = {};
         let totalWorkouts = 0;
         let totalDuration = 0;
+        let workoutTimePerDayCopy = [0, 0, 0, 0, 0, 0, 0];
         snapshot.forEach(doc => {
           const session = doc.data();
           const sessionDate = new Date(session.date.seconds * 1000);
+          const dayIndex = sessionDate.getDay();
           const year = sessionDate.getFullYear();
           const month = String(sessionDate.getMonth() + 1).padStart(2, '0');
           const day = String(sessionDate.getDate()).padStart(2, '0');
           const pstDateString = `${year}-${month}-${day}`;
+          const maxTime = Math.max(...workoutTimePerDay);
+          setMaxWorkoutTime(maxTime);
           dates[pstDateString] = { marked: true };
           totalWorkouts++;
           totalDuration += session.duration;
+          workoutTimePerDayCopy[dayIndex] += session.duration / 60;
         });
         setCompletedWorkoutDates(dates);
         setTotalWorkouts(totalWorkouts);
@@ -85,17 +96,15 @@ export default function ProgressScreen({ navigation }) {
           const firstSessionDate = Object.keys(dates).length > 0 ? new Date(Object.keys(dates)[0]) : new Date();
           const daysBetween = Math.ceil((new Date() - firstSessionDate) / (1000 * 60 * 60 * 24));
           const weeksBetween = Math.ceil(daysBetween / 7);
-          const avgWorkoutsPerWeek = totalWorkouts / weeksBetween;
-          setAvgWorkoutsPerWeek(avgWorkoutsPerWeek.toFixed(2));
 
           const avgDuration = totalDuration / totalWorkouts;
           setAvgWorkoutDuration(avgDuration / 60);
 
-          // Calculate calories burned per week
           const caloriesBurned = (totalWorkouts * 400)/ weeksBetween;
           setCaloriesBurnedPerWeek(caloriesBurned);
+
+          setWorkoutTimePerDay(workoutTimePerDayCopy);
         } else {
-          setAvgWorkoutsPerWeek(0);
           setAvgWorkoutDuration(0);
           setCaloriesBurnedPerWeek(0);
         }
@@ -103,85 +112,147 @@ export default function ProgressScreen({ navigation }) {
     return () => unsubscribe();
   }, []);
 
+  useEffect(() => {
+    const weeklyData = [];
+    const firstSessionDate = Object.keys(completedWorkoutDates).length > 0 ? new Date(Object.keys(completedWorkoutDates)[0]) : new Date();
+    const lastSessionDate = new Date();
+    const firstSunday = moment(firstSessionDate).startOf('week').toDate();
+    let weekStart = firstSunday;
+    while (weekStart <= lastSessionDate) {
+      const weekEnd = moment(weekStart).endOf('week').toDate();
+      const weekLabel = `${moment(weekStart).format('M/D')} - ${moment(weekEnd).format('M/D')}`;
+      let weeklyWorkoutCount = 0;
+      for (const date in completedWorkoutDates) {
+        const currentDate = new Date(date);
+        if (currentDate >= weekStart && currentDate <= weekEnd) {
+          weeklyWorkoutCount++;
+        }
+      }
+      weeklyData.push({ weekLabel, weeklyWorkoutCount });
+      weekStart = moment(weekStart).add(1, 'week').toDate();
+    }
+    setWeeklyWorkouts(weeklyData);
+  }, [completedWorkoutDates]);
+
   const toggleHistory = () => {
     setHistory(!showHistory);
   };
 
   return (
     <LinearBackground>
-      {loading ? (
-        <ActivityIndicator style={{ flex: 1 }} />
-      ) : (
-        <SafeAreaView>
-          <View>
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', margin: 20, marginTop: 40 }}>
-              <Text style={{ color: 'white', fontSize: 24, fontWeight: 'bold', marginLeft: 10 }}>My Progress</Text>
-              <Pressable onPress={() => navigation.navigate('Settings')}>
-                <AvatarDisplay source={userData && userData.photoURL ? { uri: userData.photoURL } : null} size={60} editable={false} clickable={false} />
-              </Pressable>
-            </View>
-            <View style={{ backgroundColor: 'rgb(34,84,150)', marginHorizontal: 125 }}>
-              <Button title="See History" color="#0094FF" onPress={toggleHistory} />
-            </View>
-            <Modal animationType="slide" visible={showHistory} onRequestClose={toggleHistory}>
-              <SafeAreaView style={styles.modalContainer}>
-                <View style={styles.notificationSettingsContainer}>
-                  <View style={styles.notificationsHeader}>
-                    <Pressable onPress={toggleHistory}>
-                      <Feather name="arrow-left" size={26} color="black" marginLeft={15} />
-                    </Pressable>
-                    <Text style={styles.notificationsHeaderText}>History</Text>
-                  </View>
-                  <CalendarList
-                    onDayPress={day => console.log('selected day', day)}
-                    markedDates={completedWorkoutDates}
+      <ScrollView>
+        {loading ? (
+          <ActivityIndicator style={{ flex: 1 }} />
+        ) : (
+          <SafeAreaView>
+            <View>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', margin: 20, marginTop: 40 }}>
+                <Text style={{ color: 'white', fontSize: 24, fontWeight: 'bold', marginLeft: 10 }}>My Progress</Text>
+                <Pressable onPress={() => navigation.navigate('Settings')}>
+                  <AvatarDisplay source={userData && userData.photoURL ? { uri: userData.photoURL } : null} size={60} editable={false} clickable={false} />
+                </Pressable>
+              </View>
+
+              <ScrollView horizontal>
+                <View style={styles.chartContainer}>
+                  <Text style={styles.chartTitle}>Total Workout Duration Per Day</Text>
+                  <BarChart
+                    data={{
+                      labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                      datasets: [{ data: workoutTimePerDay }], 
+                    }}
+                    width={350}
+                    height={220}
+                    yAxisSuffix={maxWorkoutTime <= 1 ? " min" : " min"}
+                    chartConfig={{
+                      backgroundColor: '#1cc910',
+                      backgroundGradientFrom: '#eff3ff',
+                      backgroundGradientTo: '#efefef',
+                      decimalPlaces: 0,
+                      color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                      style: { borderRadius: 15 },
+                      propsForLabels: {
+                        fontFamily: 'Arial',
+                        fontSize: 12,
+                      },
+                      barPercentage: 0.6,
+                    }}
+                    fromZero
+                    bezier
+                    style={{
+                      marginVertical: 8,
+                      borderRadius: 16,
+                    }}
                   />
                 </View>
-              </SafeAreaView>
-            </Modal>
-
-            <View style={{ alignItems: 'center', marginTop: 20 }}>
-              <Text style={styles.statText}>Total Workouts Completed: {totalWorkouts}</Text>
-              <Text style={styles.statText}>Average Workouts Per Week: {avgWorkoutsPerWeek}</Text>
-              <Text style={styles.statText}>Average Workout Duration: {avgWorkoutDuration.toFixed(2)} min/workout</Text>
-              <Text style={styles.statText}>Calories Burned Per Week: {caloriesBurnedPerWeek} cal</Text>
+                <View style={styles.chartContainer}>
+                  <Text style={styles.chartTitle}>Total Workouts Per Week</Text>
+                  <BarChart
+                    data={{
+                      labels: weeklyWorkouts.map(data => data.weekLabel),
+                      datasets: [{ data: weeklyWorkouts.map(data => data.weeklyWorkoutCount) }],
+                    }}
+                    width={Dimensions.get('window').width - 40}
+                    height={220}
+                    chartConfig={{
+                      backgroundColor: '#1cc910',
+                      backgroundGradientFrom: '#eff3ff',
+                      backgroundGradientTo: '#efefef',
+                      decimalPlaces: 0,
+                      color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                      style: { borderRadius: 15 },
+                      propsForLabels: {
+                        fontFamily: 'Arial',
+                        fontSize: 12,
+                      },
+                      barPercentage: 0.6,
+                    }}
+                    fromZero
+                    bezier
+                    style={{
+                      marginVertical: 8,
+                      borderRadius: 16,
+                    }}
+                  />
+                </View>
+              </ScrollView>
+              <View style={{ alignItems: 'center' }}>
+                <Text style={[styles.chartTitle, { marginBottom: 10, marginTop: 10 }]}>Progress Statistics </Text>
+              </View>
+              <View style={{ alignItems: 'center', marginTop: 20 }}>
+                <Text style={styles.statText}>Total Workouts Completed: {totalWorkouts}</Text>
+                <Text style={styles.statText}>Average Workout Duration: {avgWorkoutDuration.toFixed(2)} min/workout</Text>
+                <Text style={styles.statText}>Calories Burned Per Week: {caloriesBurnedPerWeek} cal</Text>
+              </View>
+              <View style={{ alignItems: 'center', marginTop: 30 }}>
+                <Pressable
+                  style={styles.button}
+                  onPress={toggleHistory}
+                >
+                  <Text style={styles.buttonText}>See History </Text>
+                  <FontAwesome name="arrow-right" size={20} color="#0044AA" />
+                </Pressable>
+              </View>
+              <Modal animationType="slide" visible={showHistory} onRequestClose={toggleHistory}>
+                <SafeAreaView style={styles.modalContainer}>
+                  <View style={styles.notificationSettingsContainer}>
+                    <View style={styles.notificationsHeader}>
+                      <Pressable onPress={toggleHistory}>
+                        <Feather name="arrow-left" size={26} color="black" marginLeft={15} />
+                      </Pressable>
+                      <Text style={styles.notificationsHeaderText}>History</Text>
+                    </View>
+                    <CalendarList
+                      onDayPress={day => console.log('selected day', day)}
+                      markedDates={completedWorkoutDates}
+                    />
+                  </View>
+                </SafeAreaView>
+              </Modal>
             </View>
-
-            {/* Display Bar Chart */}
-            <View style={styles.chartContainer}>
-              <Text style={styles.chartTitle}>Weekly Workout Summary</Text>
-              <BarChart
-                data={{
-                  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-                  datasets: [{ data: [30, 40, 35, 45, 50, 55, 40] }], // Example data for time spent (in minutes) per day
-                }}
-                width={350}
-                height={220}
-                yAxisSuffix=" min"
-                chartConfig={{
-                  backgroundColor: '#1cc910',
-                  backgroundGradientFrom: '#eff3ff',
-                  backgroundGradientTo: '#efefef',
-                  decimalPlaces: 0,
-                  color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-                  style: { borderRadius: 15 },
-                  propsForLabels: {
-                    fontFamily: 'Arial',
-                    fontSize: 12,
-                  },
-                  barPercentage: 0.6,
-                }}
-                fromZero
-                bezier
-                style={{
-                  marginVertical: 8,
-                  borderRadius: 16,
-                }}
-              />
-            </View>
-          </View>
-        </SafeAreaView>
-      )}
+          </SafeAreaView>
+        )}
+      </ScrollView>
     </LinearBackground>
   );
 }
@@ -212,16 +283,30 @@ const styles = StyleSheet.create({
   },
   statText: {
     color: 'white',
-    fontSize: 18,
+    fontSize: 15,
     marginBottom: 10,
   },
   chartContainer: {
     alignItems: 'center',
     marginTop: 20,
+    marginLeft: 10, 
+    marginRight: 10, 
   },
   chartTitle: {
     color: 'white',
     fontSize: 20,
     marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+  },
+  buttonText: {
+    color: "#0044AA",
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
